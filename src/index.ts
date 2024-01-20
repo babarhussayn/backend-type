cb5a465aff060d7d2a9230f78ed120a7c3e5f966
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-01-20 14:26:22 +0500

    change

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 
 app.post("/git-webhook", (req: Request, res: Response) => {
   const data = req.body;
-  console.log("data received \n", req.body);
+  console.log("data \n", req.body);
   res.send('Data Received..')
 });
 
