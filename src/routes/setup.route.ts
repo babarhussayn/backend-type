2eb73e9db1881c165b90b51275d3cad7399be281
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-01-27 16:12:52 +0500

    fixed dest path

--- src/routes/setup.route.ts
+++ src/routes/setup.route.ts
@@ -1,8 +1,23 @@
 import { Router, Request, Response } from "express";
 import { setup } from "../services/setup.service";
+import path from "path";
+
 const setup_router = Router();
 
 setup_router.post("/setup", async (req: Request, res: Response) => {
   const setup_data = req.body;
-  await setup(setup_data.playbook, setup_data.options);
+
+  const playbook_path = path.join(__dirname, "./../../ansible/clone_repo.yml");
+
+  const response = await setup(playbook_path, {
+    become_password: "mskhan",
+    build_dir_name: "dist",
+    dest: "/iancon/logistics/admin/client",
+    site_dir: `~/sites/config/${setup_data.site_domain}`,
+    repo_url: setup_data.repo_url,
+  });
+
+  console.log(response);
 });
+
+export default setup_router;
