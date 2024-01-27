a9c7e996daa2a40c95fb4f560aa25622e809e72e
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-01-27 16:12:34 +0500

    added setup router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { Request, Response } from "express";
+import setup_router from "./routes/setup.route";
+
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+app.use("/", setup_router);
+
 const PORT = process.env.PORT || 8000;
 
 app.post("/git-webhook", (req: Request, res: Response) => {
   const data = req.body;
   console.log("data \n", req.body);
-  res.send('Data Received..')
+  res.send("Data Received..");
 });
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
