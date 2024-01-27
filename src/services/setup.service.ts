707648a5aeaf47e55801e2f882edb435d20719c4
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-01-27 16:13:11 +0500

    fixed ansible command

--- src/services/setup.service.ts
+++ src/services/setup.service.ts
@@ -9,19 +9,35 @@ export async function setup(
     build_dir_name: string;
     become_password: string;
   }
-) {
-  const command = `ansible-playbook -e "
-                   repo_url=${options.repo_url}
-                   dest=${options.dest}
-                   site_dir=${options.site_dir}
-                   build_dir_name=${options.build_dir_name}
-                   " ${playbook} --extra-vars "ansible-become-pass=${options.become_password}"`;
+): Promise<{ success: boolean; payload: any }> {
+  let success = false;
+  let payload;
+  const command = `ansible-playbook -e  \
+                  "repo_url=${options.repo_url} \
+                   dest=${options.dest} \
+                   site_dir=${options.site_dir} \
+                   build_dir_name=${options.build_dir_name} \
+                   " ${playbook} --extra-vars ansible-become-pass=${options.become_password}`;
+
+  console.log("Running: ", command);
 
   const child_command = exec(command);
 
-  child_command.stdout?.on("data", (data) => console.log("stdout: ", data));
-  child_command.stderr?.on("data", (data) => console.log("stderr: ", data));
-  child_command.on("close", (code) =>
-    console.log("process exited with code: ", code)
-  );
+  child_command.stdout?.on("data", (data) => {
+    payload = data;
+    console.log("stdout: ", data);
+  });
+
+  child_command.stderr?.on("data", (data) => {
+    payload = data;
+    console.log("stderr: ", data);
+  });
+
+  child_command.on("close", (code) => {
+    if (code !== 0) {
+      success = true;
+    }
+  });
+
+  return { success, payload };
 }
