796e8d9d1cefe9c9a4ffed94046be8b32698acb6
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-01-27 16:11:53 +0500

    added become password

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,8 +3,9 @@
 ansible-playbook -e "repo_url=git@github.com:ianconnet/LogisticsFE-Admin.git \
                      dest=/iancon/logistics/admin/client \
                      npm_path=/snap/bin/npm \
-                     site_dir=/sites build_dir_name=dist" \
+                     site_dir=/sites build_dir_name=dist \
+                     --ansible-become-pass=mskhan " \
                      --ask-become-pass \
-                     clone_repo.yml
+                     clone_repo.yml 
 
 
