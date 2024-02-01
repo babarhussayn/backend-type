150daf6dfb8789d65e5c0cdfc49601ff203e0349
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-02-01 16:59:20 +0500

    completed frontend setup flow

--- src/routes/setup.route.ts
+++ src/routes/setup.route.ts
@@ -4,23 +4,42 @@ import path from "path";
 
 const setup_router = Router();
 
-setup_router.post("/setup", async (req: Request, res: Response) => {
+setup_router.post("/react-frontend", async (req: Request, res: Response) => {
   const setup_data = req.body;
 
+  console.log(setup_data);
   const playbook_path = path.join(__dirname, "./../../ansible/setup.yml");
+  try {
+    const response = await setup(playbook_path, {
+      become_password: "mskhan",
+      build_dir_name: "dist",
+      dest: "/clones/client",
+      site_dir: `/sites/config/${setup_data.domain}`,
+      repo_url: setup_data.git_url,
+      domain: setup_data.site_domain,
+    });
 
-  const response = await setup(playbook_path, {
-    become_password: "mskhan",
-    build_dir_name: "dist",
-    dest: "/iancon/logistics/admin/client",
-    site_dir: `~/sites/config/${setup_data.site_domain}`,
-    repo_url: setup_data.repo_url,
-  });
+    if (response.success) {
+      res.status(201).send(response);
+    }
+  } catch (err: unknown) {
+    console.log("err: ", JSON.parse((err as any).payload as string));
 
-  if (response.success) {
-    
-    console.log(response)
-    res.status(201).send(response);
+    const payload = (err as any).payload;
+    let error = payload.slice(
+      payload.indexOf("stderr_lines"),
+      payload.indexOf("stdout_lines")
+    );
+    error = error
+      .replace(`stderr_lines": ["fatal: `, "")
+      .replace(`."], "stdout":`, "")
+      .replace(/[,"':]/g, "");
+
+    res.status(500).send({
+      success: false,
+      payload,
+      error,
+    });
   }
 });
 
--- src/services/setup.service.ts
+++ src/services/setup.service.ts
@@ -1,6 +1,4 @@
-import { rejects } from "assert";
 import { exec } from "child_process";
-import { resolve } from "path";
 
 export async function setup(
   playbook: string,
@@ -10,6 +8,7 @@ export async function setup(
     site_dir: string;
     build_dir_name: string;
     become_password: string;
+    domain: string;
   }
 ): Promise<{ success: boolean; payload: string }> {
   return new Promise((resolve, rejects) => {
@@ -22,7 +21,7 @@ export async function setup(
                    build_dir_name=${options.build_dir_name} \
                    site_conf_dir=/sites/conf \
                    document_root=/dr \
-                   server_name=www.ms.com \
+                   server_name=${options.domain} \
                    port=80
                    " ${playbook} --extra-vars ansible-become-pass=${options.become_password}`;
 
@@ -30,15 +29,19 @@ export async function setup(
 
     child_command.stdout?.on("data", (data) => {
       payload += data;
+      console.log(data);
     });
 
     child_command.stderr?.on("data", (data) => {
       payload += data;
+      console.log(data);
     });
 
     child_command.on("close", function (code) {
       if (code === 0) {
         success = true;
+      } else {
+        rejects({ success, payload: JSON.stringify(payload) });
       }
 
       resolve({ success, payload: JSON.stringify(payload) });
