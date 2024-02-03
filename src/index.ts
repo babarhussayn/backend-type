4ca0b9075f94bb4cfc0e5dca38ec4f8f2a5304b9
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-02-03 12:19:42 +0500

    removed node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { Request, Response } from "express";
-import setup_router from "./routes/setup.route";
 import cors from "cors";
 
 const app = express();
@@ -11,14 +10,11 @@ app.use(
     origin: ["http://localhost:5173"],
   })
 );
-app.use("/", setup_router);
 
 const PORT = process.env.PORT || 8000;
 
-app.post("/git-webhook", (req: Request, res: Response) => {
-  const data = req.body;
-  console.log("data \n", req.body);
-  res.send("Data Received..");
+app.post("/test-route", (req: Request, res: Response) => {
+  res.send("hello");
 });
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
