525128d6604062158f37b1a481c50c96c71f3c40
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-02-26 23:22:08 +0500

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
@@ -8,12 +11,12 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-  })
+  }),
 );
+console.log(process.env.IDS_PORT);
+const PORT = process.env.IDS_PORT || 8000;
 
-const PORT = process.env.PORT || 8000;
-
-app.post("/test-route", (req: Request, res: Response) => {
+app.get("/test-route", (req: Request, res: Response) => {
   res.send("hello");
 });
 
