de5ee4da94268230a76f71422b2be5b33460d0c7
babarhussain <babarhusain262@gmail.com>
2024-11-11 12:00:03 +0500

    bcypt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,21 @@
-import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { Document, model, Schema } from "mongoose";
 
-const userSchema = new Schema(
+interface IUser extends Document {
+  name: string;
+  lname: string;
+  email: string;
+  password: string;
+  cPassword: string;
+}
+
+const userSchema = new Schema<IUser>(
   {
     name: {
+      type: String,
+      required: true,
+    },
+    lname: {
       type: String,
       required: true,
     },
@@ -11,9 +24,37 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
+    cPassword: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
-const User = model("User", userSchema);
+// export const HashPassword = async (password: string): Promise<string> => {
+//   const salt = await bcrypt.genSalt();
+//   const hash = await bcrypt.hash(password, salt);
+//   return hash;
+// };
+// export const ComparePassword = async (
+//   hash: string,
+//   password: string
+// ): Promise<boolean> => {
+//   const isMatch = await bcrypt.compare(password, hash);
+//   return isMatch;
+// };
+userSchema.pre<IUser>("save" as "save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+const User = model<IUser>("User", userSchema);
 export default User;
